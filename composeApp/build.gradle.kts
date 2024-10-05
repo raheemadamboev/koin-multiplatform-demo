b8733abb40709f790521e5003bc441dd480aa791
raheem <raheemideadev@gmail.com>
2024-10-06 00:17:48 +0500

    Installed dependencies;

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,24 +1,25 @@
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
+
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.android)
+    alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     jvm("desktop")
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,34 +30,56 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         androidMain.dependencies {
+
+            // compose preview
             implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+
+            // compose activity
+            implementation(libs.compose.activity)
+
+            // koin
+            implementation(libs.koin.android)
+            implementation(libs.koin.android.compose)
         }
+
         commonMain.dependencies {
+
+            // compose
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // viewmodel
+            implementation(libs.viewmodel)
+
+            // navigation
+            implementation(libs.navigation)
+
+            // koin
+            api(libs.koin)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
         }
+
         desktopMain.dependencies {
+
+            // compose current os
             implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
         }
     }
 }
 
 android {
     namespace = "xyz.teamgravity.koinmultiplatformdemo"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk = libs.versions.sdk.compile.get().toInt()
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
@@ -64,29 +87,32 @@ android {
 
     defaultConfig {
         applicationId = "xyz.teamgravity.koinmultiplatformdemo"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
+
     dependencies {
+
+        // compose ui tooling
         debugImplementation(compose.uiTooling)
     }
 }
--- composeApp/src/commonMain/kotlin/xyz/teamgravity/koinmultiplatformdemo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package xyz.teamgravity.koinmultiplatformdemo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
